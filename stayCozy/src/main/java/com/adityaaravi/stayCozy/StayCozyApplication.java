37bee9cd00c9e5923fa20b053ba99f799bef7b5f
Adityaa Ravi <adityaaravi6@gmail.com>
2021-12-26 14:00:27 -0800

    Added starter code from Spring Intializr

---
+++ stayCozy/src/main/java/com/adityaaravi/stayCozy/StayCozyApplication.java
@@ -0,0 +1,13 @@
+package com.adityaaravi.stayCozy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StayCozyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StayCozyApplication.class, args);
+	}
+
+}
---
+++ stayCozy/src/test/java/com/adityaaravi/stayCozy/StayCozyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adityaaravi.stayCozy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StayCozyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
